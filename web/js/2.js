ed08bd35273df4bed48cd8e1b6e33db7d65cf9cf
willy <willy@DESKTOP-2NQKV28>
2017-11-05 18:09:30 -0500

    grafico estudiante

--- web/js/2.js
+++ web/js/2.js
@@ -1,14 +1,13 @@
 
-
-window.onload = function () {
+function GPE(id) {
     var ctx = document.getElementById("popChart1");
     var myBarChart;
+
     var parametros = {
         "hidden": "1",
-        "id": "2"
+        "id": id
     };
     $.ajax({
-
         data: parametros,
         url: "ServletGraficoPE",
         type: "GET"
@@ -16,19 +15,17 @@ window.onload = function () {
     }).done(function (response) {
         console.log(response);
         var inventario = response;
-        var arreglo=[];
+        var arreglo = [];
         var arreglo1 = [];
         var arreglo2 = [];
         var arreglo3 = [];
-        for (var i = 0; i < inventario.length; i++) {
+        for (var i in inventario) {
             arreglo.push(inventario[i].valor);
             arreglo2.push(inventario[i].materia);
         }
         arreglo3.push(arreglo2);
         arreglo1.push(arreglo);
-//        $("#btn1").click(function () {
-//            $("p").append(" <b>" + arreglo1[0] + "</b>");
-//        });
+
         myBarChart = new Chart(ctx, {
             type: 'bar',
             data: {
@@ -58,5 +55,56 @@ window.onload = function () {
             }
         });
     });
+    
+}
+;
+
+
+function GTE() {
+    
+    var ctx = document.getElementById("popChart1");
+
+    var myBarChart;
+
+    var parametros = {
+        "hidden": "1"
+    };
+    $.ajax({
+
+        data: parametros,
+        url: "ServletGraficoTE",
+        type: "GET"
+    }).done(function (response) {
+
+        console.log(response);
+        src = "GraficoJS2.html";
+        var inventario = response;
+        var arreglo = [];
+        arreglo.push([inventario[0], inventario[1], inventario[2], inventario[3], inventario[4]]);
+
+        myBarChart = new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+
+                labels: ["1.0", "2.0", "3.0", "4.0", "5.0"],
+                datasets: [{
+                        label: 'notas',
+                        data: arreglo[0],
+                        backgroundColor: [
+                            'rgba(0, 99, 132, 0.6)',
+                            'rgba(30, 99, 132, 0.6)',
+                            'rgba(60, 99, 132, 0.6)',
+                            'rgba(90, 99, 132, 0.6)',
+                            'rgba(120, 99, 132, 0.6)'
+                        ]
+
+                    }]
+            },
+            options: {
+
+            }
+        });
+    }
+    );
 }
 ;
